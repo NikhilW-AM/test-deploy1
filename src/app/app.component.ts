15eca896ba34a645d2ad7c1ab52add11751a6609
NikhilW-AM <nikhil@angularminds.com>
2024-03-03 19:48:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { StripeScriptTag, StripeSource } from 'stripe-angular';
+import { loadStripe, Stripe } from '@stripe/stripe-js';
 
+const stripe = await loadStripe('pk_test_Qnd38mv665iSkePn8kzlw761');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'stripe';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('cardNumberInput') cardNumberInput!: ElementRef;
+  @ViewChild('cardExpiryInput') cardExpiryInput!: ElementRef;
+  @ViewChild('cardCvcInput') cardCvcInput!: ElementRef;
+
+  stripe!: any;
+
+  constructor() { }
+
+  ngAfterViewInit() {
+
+
+
+    this.stripe = loadStripe('pk_test_Qnd38mv665iSkePn8kzlw761');
+
+    const elements = this.stripe.elements();
+
+    const cardNumber = elements.create('cardNumber');
+    cardNumber.mount(this.cardNumberInput.nativeElement);
+
+    const cardExpiry = elements.create('cardExpiry');
+    cardExpiry.mount(this.cardExpiryInput.nativeElement);
+
+    const cardCvc = elements.create('cardCvc');
+    cardCvc.mount(this.cardCvcInput.nativeElement);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StripeModule } from "stripe-angular";
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StripeModule.forRoot('pk_test_Qnd38mv665iSkePn8kzlw761'),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
